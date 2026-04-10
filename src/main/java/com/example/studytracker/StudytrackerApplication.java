81091bda387241e526ed39fc136ae8e70eecaf53
yuzuasan <showdragonvm77773@outlook.jp>
2026-04-10 15:05:10 +0900

    初回コミット

---
+++ src/main/java/com/example/studytracker/StudytrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.studytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudytrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudytrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studytracker/StudytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudytrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
